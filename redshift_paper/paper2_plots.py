289c72241590e3b51777fc517140486a28d757e9
jkadowaki <jkadowaki@email.arizona.edu>
2019-06-21 01:18:50 -0700

    Updated data w/ UDGs observed in 2019 + Added Statistical tests (t-test, ks-test) to compare isolated/companion populations + Updated Plot formatting

--- redshift_paper/paper2_plots.py
+++ redshift_paper/paper2_plots.py
@@ -10,8 +10,13 @@ plt.rc('text', usetex=True)
 import numpy as np
 import os
 import pandas as pd
+from scipy import stats
 import seaborn as sns
-sns.set(font_scale=1.5, rc={'text.usetex' : True}, style="dark", color_codes=True)
+sns.set(font_scale=25, rc={'text.usetex':True}, style="darkgrid", color_codes=True)
+sns.set_style("darkgrid", {"legend.frameon":True})
+
+hist_color = 'Blue'
+hist_idx   = -1
 
 ################################################################################
 
@@ -107,35 +112,16 @@ def convert_name_to_coord(name):
 
     return round(new_ra, 6), -round(new_dec, 6)
 
-################################################################################
-    """
-    # SMUDGES Survey Data
-    redshift_file = 'results.tsv'
-    smudges_file  = 'smudges.txt'
-    
-    smudges = pd.read_table( os.path.join(directory, smudges_file),
-                             skiprows=35,
-                             delim_whitespace=True,
-                             names=('SMDG_Name', 'ra', 'dec', 'mu_0_g', 'mu_0_r',
-                                    'mu_0_z', 'err_mu_g', 'err_mu_r', 'err_mu_z',
-                                    're', 'err_re', 'b/a', 'err_b/a', 'theta',
-                                    'err_theta', 'M_g', 'M_r', 'M_z', 'err_Mg',
-                                    'err_Mr', 'err_Mz' ))
 
-    # Redshift Measurements
-    results = pd.read_csv( os.path.join(directory, redshift_file), sep='\t')
-
-    # cz-DataFrame
-    df_cz = results[ ~pd.isna(results['cz']) ][['SMDG_Name','cz','cz_err']]
-    """
 ################################################################################
 
 def main(data_directory='.',
          plot_directory='.',
          data_file='kadowaki19_redshifts.tsv',
          update_dat_file=False,
-         plot_pair=True,
-         plot_hist=False):
+         plot_pair=False,
+         plot_hist=False,
+         plot_ks=True):
     
     """
     Args:
@@ -153,6 +139,7 @@ def main(data_directory='.',
     redshift_plot  = os.path.join(plot_directory, 'cz_hist.pdf')
     axisratio_plot = os.path.join(plot_directory, 'reff_axisratio.pdf')
     pair_plot      = os.path.join(plot_directory, 'pair.pdf')
+    ks_plot        = os.path.join(plot_directory, 'ks_test.pdf')
 
     # Load Data
     df_results = read_data(results_file)
@@ -166,7 +153,8 @@ def main(data_directory='.',
     iso = ["Isolated" if val==0 else "Too Close" if val==-1 else "Companions"
            for val in df_results['NUM500']]
     df_results['Environment'] = iso
-    
+
+    color = 'Red'
 
     ############################################################################
 
@@ -197,15 +185,17 @@ def main(data_directory='.',
     
     ############################################################################
     
-    def remove_repeating_lists(args):
+    def remove_repeating_lists(args, verbose=False):
         
         new_args = []
         
         for a in args:
             if np.size(np.unique(a)) > 1:
                 new_args.append(a)
-    
-        print(new_args)
+
+        if verbose:
+            print(new_args)
+
         return new_args
     
     ############################################################################
@@ -214,10 +204,20 @@ def main(data_directory='.',
         pass
     
     ############################################################################
-    
-    def hist(*args,**kwargs):
+
+    def change_color():
+
+        global hist_color
+
+        if hist_color == 'Red':
+            hist_color = 'Blue'
+        else:
+            hist_color = 'Red'
+
+
+    def hist(*args, **kwargs):
         print("\nhist")
-        
+
         new_args = remove_repeating_lists(remove_nan(args))
         large_args = []
         min_y, max_y = 9999999, -9999999
@@ -230,16 +230,35 @@ def main(data_directory='.',
                     min_y = min(a)
                 if max(a) > max_y:
                     max_y = max(a)
-    
+
+        print(large_args)
+
         if len(large_args):
-            dist = sns.distplot(*large_args)
-            axes = dist.axes
-            axes.set_ylim(min_y, max_y)
+
+            hist_bins = np.linspace(min(a), max(a), 6)
+            dist = sns.distplot(*large_args, rug=True, kde=True, hist=False, norm_hist=True, color=hist_color,  bins = hist_bins)
+            sns.distplot(*large_args, kde=False, hist=True, norm_hist=True, color=hist_color, bins=hist_bins)
+
+            axes      = dist.axes
+            hist_val  = np.histogram(*large_args, bins=hist_bins, density=True)[0]
+            ylimit    = np.max(hist_val)
+            curr_ylim = axes.get_ylim()[1]
+
+            if curr_ylim > 5*ylimit or ylimit > curr_ylim:
+                axes.set_ylim(0, ylimit/0.8)
+                #ax2 = axes.twinx()
+                #ax2.set_ylim(0, ylimit/0.8)
+                #ax2.ytick
+
+            axes.xaxis.set_tick_params(labelsize=50)
+            axes.yaxis.set_tick_params(labelsize=50)
+
+            change_color()
 
     ############################################################################
 
     def scatter(*args,**kwargs):
-        plt.scatter(*args, **kwargs, marker='.')
+        plt.scatter(*args, **kwargs, s=15, edgecolor='k', linewidth=0.001)
         
     ############################################################################
 
@@ -247,7 +266,8 @@ def main(data_directory='.',
     
         sns.set(style="ticks", color_codes=True)
         features = ["cz", "Reff", "MUg0", "Mg", "b/a", "NUM500", "Environment"]
-        
+
+        markers   = ['^', 'x', 'o']
         col_list  = ['Blue',  'Green',  'Red']
         cmap_list = ['Blues', 'Greens', 'Reds']
         env_list  = df_results["Environment"].unique()
@@ -258,8 +278,10 @@ def main(data_directory='.',
         
         ax = sns.PairGrid(data=df_results[features],
                           hue="Environment",
-                          palette=col_dict)
-        
+                          palette=col_dict,
+                          diag_sharey=False,
+                          hue_kws={"marker":markers})
+
         ############################################################################
         
         def contours(*args,**kwargs):
@@ -271,18 +293,37 @@ def main(data_directory='.',
                 label = df_results["Environment"].iloc[idx]
                 cmap  = cmap_dict.get(label)
                 print(idx, label, cmap)
-                
-                sns.kdeplot(*new_args, **kwargs, cmap=cmap, shade_lowest=True)
+
+                if idx != 1:  # Exclude Too Close
+                    sns.kdeplot(*new_args, **kwargs, cmap=cmap, shade_lowest=True)
         
         ############################################################################
-        
-        
+
+
         ax.map_diag(hist)
         ax.map_lower(contours)
         ax.map_upper(scatter)
-        ax.add_legend()
-        
 
+
+        # LEGEND LABELS
+        env_replacements = {"Companions":r"$\mathrm{Companions}$",
+                            "Isolated":r"$\mathrm{Isolated}$",
+                            "Too Close":r"$cz<1000 \, \mathrm{km/s}$"}
+
+        # Replace Current Labels for LaTeX Labels
+        labels = [env_replacements[env_label] for env_label in ax._legend_data.keys()]
+
+        # LEGEND HANDLES
+        handles = ax._legend_data.values()
+
+        # ADD LEGEND & Fix Placement
+        ax.fig.legend(handles=handles, labels=labels, loc='lower center', ncol=3, fontsize=15,
+                      frameon=True, edgecolor='k', markerscale=2.5,
+                      title=r'$\mathrm{Environment}$', title_fontsize=20)
+        ax.fig.subplots_adjust(top=1.05, bottom=0.12)
+
+
+        # AXIS LABELS
         replacements = {"cz":r'$cz \, \left( \mathrm{km/s} \right)$',
                         "Reff":r'$R_\mathrm{eff} \, \left( \mathrm{kpc} \right)$',
                         "MUg0":r'$\mu \left(g,0\right) \, \left( \mathrm{mag} \, \mathrm{arcsec}^{-2} \right)$',
@@ -293,15 +334,18 @@ def main(data_directory='.',
 
         for x_idx in range(len(features)-1):
             for y_idx in range(len(features)-1):
-                
+
+                ax.axes[x_idx][y_idx].tick_params(labelsize=15)
+
                 xlabel = ax.axes[x_idx][y_idx].get_xlabel()
                 ylabel = ax.axes[x_idx][y_idx].get_ylabel()
                 
                 if xlabel in replacements.keys():
-                    ax.axes[x_idx][y_idx].set_xlabel(replacements[xlabel])
+                    ax.axes[x_idx][y_idx].set_xlabel(replacements[xlabel], fontsize=20)
                 if ylabel in replacements.keys():
-                    ax.axes[x_idx][y_idx].set_ylabel(replacements[ylabel])
-    
+                    ax.axes[x_idx][y_idx].set_ylabel(replacements[ylabel], fontsize=20)
+
+
         # Save & Display Figure
         plt.savefig(pair_plot, bbox_inches = 'tight')
 
@@ -345,6 +389,39 @@ def main(data_directory='.',
         with open(coords_file, 'w') as f:
             for t in coords_list:
                 f.write('\t'.join(str(s) for s in t) + '\n')
+
+
+    ############################################################################
+
+    if plot_ks:
+
+        df_isolated  = df_results.loc[df_results["Environment"] == "Isolated"]
+        df_companion = df_results.loc[df_results["Environment"] == "Companions"]
+
+        fig, axes = plt.subplots(2, 3) #, subplot_kw=dict(polar=True))
+
+        for idx, feature in enumerate(["cz", "Reff", "MUg0", "Mg", "b/a", "NUM500"]):
+
+            #print("\n", df_isolated[feature].dropna())
+            #print(df_companion[feature].dropna())
+
+            t_stat, t_pval = stats.ttest_ind(df_isolated[feature].dropna(),
+                                             df_companion[feature].dropna(),
+                                             equal_var=False)
+
+            ks_stat, ks_pval = stats.ks_2samp(df_isolated[feature].dropna(),
+                                              df_companion[feature].dropna(),
+                                              alternative="two-sided")
+
+            print("\nFeature:",       feature)
+            print("T-statistic:",     t_stat)
+            print("P-value (2tail):", t_pval)
+            print("KS-statistic:",    ks_stat)
+            print("P-value (2tail):", ks_pval)
+
+            axes[int(np.floor(idx/2)), idx%3].plot(stats.cumfreq) =
+            axes[int(np.floor(idx/2)), idx%3].set_title(feature)
+
 
 ################################################################################
 
